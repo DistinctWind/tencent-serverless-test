965646b2358db61d1f1c01a63deb61925b2260ea
gorun <307532500@qq.com>
2024-09-07 19:19:24 +0800

    Version 1

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,9 @@ export default function Home() {
           height={38}
           priority
         />
+        <h1 className={"text-2xl font-bold"}>
+          [Version 1] This build is for tencent serverless test.
+        </h1>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
